91dd3c3e2ababf8b615c3dc3dc5671870e4a5de8
Dom.Hofer <dmand6666@gmail.com>
2021-05-03 16:20:54 +0200

    Setup of the 2 microservices

---
+++ druckerei-service/src/test/java/at/itkollegimst/hofer/pos1makro/test2/buchhandlung/DruckereiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.itkollegimst.hofer.pos1makro.test2.buchhandlung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruckereiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
