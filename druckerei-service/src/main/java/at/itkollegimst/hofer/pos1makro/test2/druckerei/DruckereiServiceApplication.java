cc016346ed9290173f1043b017e3310731225a8d
Dom.Hofer <dmand6666@gmail.com>
2021-05-10 17:51:53 +0200

    added stuff that i forgot to comit?

---
+++ druckerei-service/src/main/java/at/itkollegimst/hofer/pos1makro/test2/druckerei/DruckereiServiceApplication.java
@@ -0,0 +1,13 @@
+package at.itkollegimst.hofer.pos1makro.test2.druckerei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruckereiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DruckereiServiceApplication.class, args);
+	}
+
+}
