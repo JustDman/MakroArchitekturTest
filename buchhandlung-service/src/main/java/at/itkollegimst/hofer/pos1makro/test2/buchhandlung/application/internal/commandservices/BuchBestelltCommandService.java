14b891e7845e73d66a70d423d22da2906d0c370b
Dom.Hofer <dmand6666@gmail.com>
2021-05-11 21:49:06 +0200

    added the long lost assets🎉🍺

--- buchhandlung-service/src/main/java/at/itkollegimst/hofer/pos1makro/test2/buchhandlung/application/internal/commandservices/BuchBestelltCommandService.java
+++ buchhandlung-service/src/main/java/at/itkollegimst/hofer/pos1makro/test2/buchhandlung/application/internal/commandservices/BuchBestelltCommandService.java
@@ -8,7 +8,6 @@ import at.itkollegimst.hofer.pos1makro.test2.buchhandlung.infastructure.reposito
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
